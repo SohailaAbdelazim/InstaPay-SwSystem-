90dccd039da7bbae700f4d9ad4990d3ba8bb9217
Shawky Ebrahim Ahmed <101745968+shawkyebrahim2514@users.noreply.github.com>
2023-11-11 00:02:26 +0200

    Main Structure

---
+++ Instapay/src/User.java
@@ -0,0 +1,13 @@
+public class User {
+    private String username;
+    private String mobileNumber;
+    private Double balance;
+    private String name;
+    private String bankAccountNumber;
+    private String walletNumber;
+    public void deductAmount(Double amount){}
+    public Boolean compareBalance(Double amount){
+        return null;
+    }
+
+}
